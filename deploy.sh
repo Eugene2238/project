7f851cb2b62db29992507b6c20051f74a305796a
evgenii <eugene2238@gmail.com>
2019-07-28 23:24:39 -0700

    Deployed 0.0.4

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 kubectl apply -f k8s
 
-kubectl set image deployments/backend-deployment backend=eugene2238/project-backend:0.0.3
+kubectl set image deployments/backend-deployment backend=eugene2238/project-backend:0.0.4
