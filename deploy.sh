0805c0cc8b640dcf5c144190a48e168f81ce4352
evgenii <eugene2238@gmail.com>
2019-07-28 20:29:24 -0700

    Deploy 0.0.3 project version

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 kubectl apply -f k8s
 
-kubectl set image deployments/backend-deployment backend=eugene2238/project-backend:0.0.2
+kubectl set image deployments/backend-deployment backend=eugene2238/project-backend:0.0.3
